88a6a4e6bc520e7f3f3689befce440c88b88bf0c
dima1207 <palahantzidis.d@gmail.com>
2021-07-08 09:12:04 -0700

    My second file

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Constants {
+
+    public static final String APPLICATION_JSON = "application/json";
+        public static final String Accept = "Accept";
+         public static final String CONTENT_TYPE = "Content-Type";
+
+
+
+
+
+
+}
